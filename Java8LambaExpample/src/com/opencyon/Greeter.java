486659e001010129b87a98f1c020ab6afa77c352
admin1 <admin1@admin1-desktop>
2019-05-02 17:14:12 +0530

    modified class Greeter

--- Java8LambaExpample/src/com/opencyon/Greeter.java
+++ Java8LambaExpample/src/com/opencyon/Greeter.java
@@ -5,7 +5,15 @@ public class Greeter {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Add a = new Add();
+		int add = a.addNumber(10, 30);
+		System.out.println(add);
+		
 	}
 
+}
+class Add{
+	public int addNumber(int x, int y) {
+		return x+y;
+	}
 }
